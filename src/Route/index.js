8dfa141468dc98d31c26833cce3f86022af9f409
manovijay1997 <vijaysm97@gmail.com>
2019-09-07 16:25:19 +0530

    add welcome

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Welcome from "../Pages/Welcome/Welcome.js"
 import Home from "../Pages/Home/Home.js";
 import Blog from "../Pages/Blog/Blog.js"
 class RoutePage extends Component {
@@ -8,8 +9,9 @@ class RoutePage extends Component {
         return (
             <Router>
                 <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/blog' component={Blog} />
+                    <Route exact path='/' component={Welcome} />
+                    <Route path='/home' component={Home} />
+                    <Route path='/blog' component={Blog} />
                 </Switch>
             </Router>
         );
