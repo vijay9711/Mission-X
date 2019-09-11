52f5d084e3969924ddbc6cc371523e7b36899b2b
manovijay1997 <vijay.m@diatoz.com>
2019-09-11 18:01:01 +0530

    add typed in domain show

--- src/Pages/Welcome/Welcome.js
+++ src/Pages/Welcome/Welcome.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Welcome.css";
 import $ from "jquery";
+import Typed from "react-typed";
 
 class Welcome extends Component {
   state = {};
@@ -10,13 +11,30 @@ class Welcome extends Component {
   showName = event => {
     let name = $("#MyName").text();
     console.log("vijay", name);
+    // $(".kayo-work").typed({
+    //   strings: ["Web Developer.", "Web Designer.", "Software developer"],
+    //   cursorChar: "",
+    //   typeSpeed: 100,
+    //   loop: true,
+    //   backSpeed: 50
+    // });
   };
   render() {
     return (
       <div className="welcome">
-        <p id="MyName" className="name">
-          I'm Vijay
-        </p>
+        <div className="Developer-intro">
+          <p id="MyName" className=" m-0 p-0">
+            Hi, I'm
+          </p>
+          <p className="Developer-name m-0 p-0">Vijay</p>
+          <Typed
+            strings={["Software Developer", "web developer"]}
+            className="typed-content"
+            typeSpeed={70}
+            backSpeed={30}
+            loop
+          />
+        </div>
       </div>
     );
   }
